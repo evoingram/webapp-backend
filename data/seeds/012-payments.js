3ba79e6b07f080926c73b302855311994b3a1613
Erica L. Ingram <inquiries@aquoco.co>
2020-03-08 15:07:44 -0700

    fixed date formats

--- data/seeds/012-payments.js
+++ data/seeds/012-payments.js
@@ -8,31 +8,31 @@ exports.seed = function(knex) {
 				{
 					pid: 1,
 					iid: 1,
-					remitdate: Date.UTC('07-01-2019'),
+					remitdate: Date.UTC({ year: 2020, month: 07, date: 01 }),
 					amount: 50.0
 				},
 				{
 					pid: 2,
 					iid: 2,
-					remitdate: Date.UTC('08-01-2019'),
+					remitdate: Date.UTC({ year: 2020, month: 08, date: 01 }),
 					amount: 75.6
 				},
 				{
 					pid: 3,
 					iid: 3,
-					remitdate: Date.UTC('09-01-2019'),
+					remitdate: Date.UTC({ year: 2020, month: 09, date: 01 }),
 					amount: 125.35
 				},
 				{
 					pid: 4,
 					iid: 4,
-					remitdate: Date.UTC('10-01-2019'),
+					remitdate: Date.UTC({ year: 2020, month: 10, date: 01 }),
 					amount: 545.9
 				},
 				{
 					pid: 5,
 					iid: 5,
-					remitdate: Date.UTC('11-01-2019'),
+					remitdate: Date.UTC({ year: 2020, month: 11, date: 01 }),
 					amount: 2498.6
 				}
 			]);
--- data/seeds/019-communicationhistory.js
+++ data/seeds/019-communicationhistory.js
@@ -8,35 +8,35 @@ exports.seed = function(knex) {
 					courtdatesid: 1,
 					customersid: 1,
 					filepath: '/sample/path/1',
-					datecreated: Date.UTC('05-15-2019')
+					datecreated: Date.UTC({ year: 2020, month: 05, date: 15 })
 				},
 				{
 					chid: 2,
 					courtdatesid: 2,
 					customersid: 2,
 					filepath: '/sample/path/2',
-					datecreated: Date.UTC('06-15-2019')
+					datecreated: Date.UTC({ year: 2020, month: 06, date: 15 })
 				},
 				{
 					chid: 3,
 					courtdatesid: 3,
 					customersid: 3,
 					filepath: '/sample/path/3',
-					datecreated: Date.UTC('07-15-2019')
+					datecreated: Date.UTC({ year: 2020, month: 07, date: 15 })
 				},
 				{
 					chid: 3,
 					courtdatesid: 4,
 					customersid: 4,
 					filepath: '/sample/path/4',
-					datecreated: Date.UTC('08-15-2019')
+					datecreated: Date.UTC({ year: 2020, month: 08, date: 15 })
 				},
 				{
 					chid: 3,
 					courtdatesid: 5,
 					customersid: 5,
 					filepath: '/sample/path/5',
-					datecreated: Date.UTC('09-15-2019')
+					datecreated: Date.UTC({ year: 2020, month: 09, date: 15 })
 				}
 			]);
 		});
