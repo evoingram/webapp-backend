a830737f32d0ed78919b202e328368916d030e1e
Erica L. Ingram <inquiries@aquoco.co>
2020-03-08 13:11:52 -0700

    fixed various keys in migrations

--- data/migrations/001-customers.js
+++ data/migrations/001-customers.js
@@ -12,19 +12,19 @@ exports.up = function(knex) {
 			.notNullable()
 			.unique();
 
-		customers.string('password', 128).notNullable();
-		customers.string('company', 128).notNullable();
-		customers.string('mrms', 128).notNullable();
-		customers.string('lastname', 128).notNullable();
-		customers.string('firstname', 128).notNullable();
-		customers.string('jobtitle', 128);
-		customers.string('businessphone', 128);
-		customers.string('address1', 128);
-		customers.string('address2', 128);
-		customers.string('city', 128);
-		customers.string('state', 128);
-		customers.string('zip', 128);
-		customers.string('notes', 128);
+		customers.string('password').notNullable();
+		customers.string('company').notNullable();
+		customers.string('mrms').notNullable();
+		customers.string('lastname').notNullable();
+		customers.string('firstname').notNullable();
+		customers.string('jobtitle');
+		customers.string('businessphone');
+		customers.string('address1');
+		customers.string('address2');
+		customers.string('city');
+		customers.string('state');
+		customers.string('zip');
+		customers.string('notes');
 		customers
 			.boolean('factoring')
 			.notNullable()
@@ -35,5 +35,3 @@ exports.up = function(knex) {
 exports.down = function(knex) {
 	return knex.schema.dropTableIfExists('customers');
 };
-
-postgres://ktiszgxbfnxagz:c59295f2e001bdc3e63cf83c72ac08f8bdff7086d1be32965f87730e20f1067e@ec2-184-72-235-159.compute-1.amazonaws.com:5432/d6gqnc1s8u40m9
