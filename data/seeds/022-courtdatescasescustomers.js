83b3b57090082a3554e3f2256a589bfdbb267c8d
Erica L. Ingram <inquiries@aquoco.co>
2020-03-08 15:37:31 -0700

    fixed date formats

--- data/seeds/022-courtdatescasescustomers.js
+++ data/seeds/022-courtdatescasescustomers.js
@@ -5,11 +5,11 @@ exports.seed = function(knex) {
 		.then(function() {
 			// Inserts seed entries
 			return knex('courtdatescasescustomers').insert([
-				{ id: 1, casesid: 1, courtdatesid: 1, orderingid: 1 },
-				{ id: 2, casesid: 2, courtdatesid: 2, orderingid: 2 },
-				{ id: 3, casesid: 3, courtdatesid: 3, orderingid: 3 },
-				{ id: 4, casesid: 4, courtdatesid: 4, orderingid: 4 },
-				{ id: 5, casesid: 5, courtdatesid: 5, orderingid: 5 }
+				{ cdccid: 1, casesid: 1, courtdatesid: 1, orderingid: 1 },
+				{ cdccid: 2, casesid: 2, courtdatesid: 2, orderingid: 2 },
+				{ cdccid: 3, casesid: 3, courtdatesid: 3, orderingid: 3 },
+				{ cdccid: 4, casesid: 4, courtdatesid: 4, orderingid: 4 },
+				{ cdccid: 5, casesid: 5, courtdatesid: 5, orderingid: 5 }
 			]);
 		});
 };
