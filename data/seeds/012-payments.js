dc63efcb1dee2e666c84553232264a96be3b8c1d
Erica L. Ingram <inquiries@aquoco.co>
2020-03-08 14:10:53 -0700

    fixed various keys in migrations

--- data/seeds/012-payments.js
+++ data/seeds/012-payments.js
@@ -7,31 +7,31 @@ exports.seed = function(knex) {
 			return knex('payments').insert([
 				{
 					pid: 1,
-					invoiceno: 594000,
+					iid: 1,
 					remitdate: '07/01/2019',
 					amount: 50.0
 				},
 				{
 					pid: 2,
-					invoiceno: 594001,
+					iid: 2,
 					remitdate: '08/01/2019',
 					amount: 75.6
 				},
 				{
 					pid: 3,
-					invoiceno: 594002,
+					iid: 3,
 					remitdate: '09/01/2019',
 					amount: 125.35
 				},
 				{
 					pid: 4,
-					invoiceno: 594003,
+					iid: 4,
 					remitdate: '10/01/2019',
 					amount: 545.9
 				},
 				{
 					pid: 5,
-					invoiceno: 594004,
+					iid: 5,
 					remitdate: '11/01/2019',
 					amount: 2498.6
 				}
