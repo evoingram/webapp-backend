88637eb8730b48712065d6ef61b514ea020f61c5
Erica L. Ingram <inquiries@aquoco.co>
2020-03-30 17:38:51 -0700

    designed returned object of one single job number

---
+++ docs/object-design-examples.js
@@ -0,0 +1,241 @@
+/*
+Data Design, main endpoint /jobnumber
+returned object of one single job number
+*/
+let object = {
+	'job Number': {
+		general: {
+			courtdatesid: '',
+			ttid: '',
+			turnaround: '',
+			hearingdate: '',
+			starttime: '',
+			endtime: '',
+			audiolength: '',
+			location: '',
+			duedate: '',
+			filed: '',
+			hearingtitle: '',
+			judgename: '',
+			judgetitle: '',
+			'': '',
+			'': '',
+			'': '',
+			'': ''
+		},
+		financial: {
+			invoiceno: '',
+			rate: '',
+			ratesid: '',
+			brandingtheme: '',
+			btid: '',
+			paymenttype: '',
+			factoringcost: '',
+			estimatedquantity: '',
+			actualquantity: '',
+			subtotal: '',
+			finalprice: '',
+			estimatedadvancedate: '',
+			estimatedrebatedate: '',
+			ppid: '',
+			ppstatus: '',
+			discount: '',
+			reference: '',
+			invoicedate: '',
+			duedate: '',
+			itemcode: '',
+			description: '',
+			accountcode: '',
+			taxtype: '',
+			expenses: {
+				expense1: {
+					eid: '',
+					vendor: '',
+					date: '',
+					amount: '',
+					description: ''
+				}
+			},
+			payments: {
+				payment1: {
+					pid: '',
+					amount: '',
+					remitdate: ''
+				}
+			},
+			'': ''
+		},
+		case: {
+			party1: 'Ricky Martin',
+			party1name: 'Petitioner',
+			party2: 'Michael Buble',
+			party2name: 'Respondent',
+			casenumber1: '13-5-04965-3 SEA',
+			casenumber2: '00000-0',
+			jurisdiction: 'King County Superior Court',
+			notes: 'notes'
+		},
+		shipping: {
+			package1: {
+				shipdate: '',
+				trackingno: '',
+				soid: '',
+				mcid: '',
+				mailclass: '',
+				ptid: '',
+				packagetype: '',
+				customerid: '',
+				amount: '',
+				shippingcost: '',
+				width: '',
+				length: '',
+				height: '',
+				prioritymailexpress1030: false,
+				holidaydelivery: false,
+				sundaydelivery: false,
+				saturdaydelivery: false,
+				signaturerequired: false,
+				stealth: false,
+				replypostage: false,
+				insuredmail: false,
+				cod: false,
+				restricteddelivery: false,
+				adultsigrestricted: false,
+				adultsigrequired: false,
+				returnreceipt: false,
+				certifiedmail: false,
+				sigconfirmation: false,
+				uspstracking: false,
+				reference: 1,
+				value: 100,
+				description: 'transcripts',
+				weightoz: 10,
+				output: ''
+			},
+			package2: {
+				shipdate: '',
+				trackingno: '',
+				soid: '',
+				mcid: '',
+				mailclass: '',
+				ptid: '',
+				packagetype: '',
+				customerid: '',
+				amount: '',
+				shippingcost: '',
+				width: '',
+				length: '',
+				height: '',
+				prioritymailexpress1030: false,
+				holidaydelivery: false,
+				sundaydelivery: false,
+				saturdaydelivery: false,
+				signaturerequired: false,
+				stealth: false,
+				replypostage: false,
+				insuredmail: false,
+				cod: false,
+				restricteddelivery: false,
+				adultsigrestricted: false,
+				adultsigrequired: false,
+				returnreceipt: false,
+				certifiedmail: false,
+				sigconfirmation: false,
+				uspstracking: false,
+				reference: 1,
+				value: 100,
+				description: 'transcripts',
+				weightoz: 10,
+				output: ''
+			}
+		},
+		appearances: {
+			speakerlist: [],
+			ordering: {
+				customersid: '',
+				factoring: '',
+				company: '',
+				mrms: '',
+				lastname: '',
+				firstname: '',
+				email: '',
+				jobtitle: '',
+				businessphone: '',
+				address1: '',
+				address2: '',
+				city: '',
+				state: '',
+				zip: '',
+				notes: ''
+			},
+			appearance1: {
+				customersid: '',
+				factoring: '',
+				company: '',
+				mrms: '',
+				lastname: '',
+				firstname: '',
+				email: '',
+				jobtitle: '',
+				businessphone: '',
+				address1: '',
+				address2: '',
+				city: '',
+				state: '',
+				zip: '',
+				notes: ''
+			},
+			appearance2: {
+				customersid: '',
+				factoring: '',
+				company: '',
+				mrms: '',
+				lastname: '',
+				firstname: '',
+				email: '',
+				jobtitle: '',
+				businessphone: '',
+				address1: '',
+				address2: '',
+				city: '',
+				state: '',
+				zip: '',
+				notes: ''
+			}
+		},
+		citations: {
+			citation1: {
+				findcitation: '',
+				replacehyperlink: '',
+				longcitation: '',
+				chcategory: '',
+				webaddress: ''
+			},
+			citation2: {
+				findcitation: '',
+				replacehyperlink: '',
+				longcitation: '',
+				chcategory: '',
+				webaddress: ''
+			}
+		},
+		commHistory: {
+			comm1: {
+				chid: '',
+				filepath: '',
+				datecreated: '',
+				customersid: ''
+			},
+			comm2: {
+				chid: '',
+				filepath: '',
+				datecreated: '',
+				customersid: ''
+			}
+		},
+		agShortcuts: {
+			ag1: '',
+			ag2: ''
+		}
+	}
+};
