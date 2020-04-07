7ffbf9ff594a192384db9a1dc3993b2491fc5138
Erica L. Ingram <inquiries@aquoco.co>
2020-04-07 03:21:30 -0700

    undoing previous change, going with what works

--- financials/ratesModel.js
+++ financials/ratesModel.js
@@ -25,52 +25,16 @@ async function add(rate) {
 function findById(ratesid) {
 	return db('rates').select('ratesid', '*').where({ ratesid }).first();
 }
-/*
 function update(ratesid, rate) {
 	return db('rates').where('ratesid', Number(ratesid)).update(rate);
 }
-*/
+/*
 async function update(ratesid, rate) {
 	const [ratesidA] = await db('rates').update(rate);
 	return findById(ratesidA);
 }
+*/
 
 function remove(ratesid) {
 	return db('rates').where('ratesid', Number(ratesid)).del();
 }
-
-/*
-
-
-
-*/
-
-/*
-
-
-
-*/
-
-/*
-
-
-
-*/
-
-/*
-
-
-
-*/
-
-/*
-
-
-
-*/
-
-/*
-
-
-
-*/
